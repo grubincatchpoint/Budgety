89958abcd88ef76482cb35f01bc664382f1f968c
grubincatchpoint <grubin@catchpoint.com>
2017-03-09 18:01:39 -0500

    Update app.js
    Format numbers!

--- app.js
+++ app.js
@@ -128,6 +128,18 @@ var DOMstrings = {
   container: '.container',
   expensesPercLabel: '.item__percentage'
 };
+var formatNumber = function(num, type) {
+      var numSplit, int, dec;
+      num = Math.abs(num);
+      num = num.toFixed(2);
+      numSplit = num.split('.');
+      int = numSplit[0];
+      dec = numSplit[1];
+      if (int.length > 3) {
+        int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, 3);
+      }
+      return (type === 'expense' ? '-' : '+') + ' ' + int + '.' + dec;
+    };
 
   return {
     getInput: function() {
@@ -150,7 +162,7 @@ var DOMstrings = {
       // Replace placeholder text with some actual data
       newHtml = html.replace('%id%', obj.id);
       newHtml = newHtml.replace('%description%', obj.description);
-      newHtml = newHtml.replace('%value%', obj.value);
+      newHtml = newHtml.replace('%value%', formatNumber(obj.value, type));
       // Insert the HTML into DOM
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
     },
@@ -168,9 +180,10 @@ var DOMstrings = {
       fieldsArr[0].focus();
     },
     displayBudget: function(obj) {
-      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
-      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalIncome;
-      document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExpense;
+      obj.budget > 0 ? type = 'income' : type = 'expense';
+      document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(obj.budget, type);
+      document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(obj.totalIncome, 'income');
+      document.querySelector(DOMstrings.expensesLabel).textContent = formatNumber(obj.totalExpense, 'expense');
       if (obj.percentage > 0) {
         document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
       } else {
